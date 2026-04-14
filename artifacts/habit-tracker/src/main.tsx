b56dcd351202f1a141a660d39d67e00359dda516
SethSystem <168155908+SethSystem@users.noreply.github.com>
2026-04-14 18:01:09 -0300

    Refactor main.tsx to use new providers and remove SW
    Removed service worker registration and added QueryClientProvider with TooltipProvider and WouterRouter.

--- artifacts/habit-tracker/src/main.tsx
+++ artifacts/habit-tracker/src/main.tsx
@@ -1,18 +1,8 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(
-      (registration) => {
-        console.log('SW registrado, escopo:', registration.scope);
-      },
-      (err) => {
-        console.log('Falha ao registrar SW:', err);
-      }
-    );
-  });
-}
-
-createRoot(document.getElementById("root")!).render(<App />);
+<QueryClientProvider client={queryClient}>
+  <TooltipProvider>
+    <WouterRouter>
+      <Router />
+    </WouterRouter>
+    <Toaster />
+  </TooltipProvider>
+</QueryClientProvider>
